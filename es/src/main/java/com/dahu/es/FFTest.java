c7f3bddcf8a72496dda5425a64ced3f475e0081e
ysh <214785256@qq.com>
2018-05-29 11:26:47 +0800

    撒F阿飞

---
+++ es/src/main/java/com/dahu/es/FFTest.java
@@ -0,0 +1,19 @@
+package com.dahu.es;
+
+/**
+ * Created by zzh on 2018/5/24.
+ */
+public class FFTest {
+
+  public static void main(String[] args) {
+    String ss = "hello";
+    ff(ss);
+    System.out.println(ss);
+  }
+
+  public static void ff(String str){
+    str= str+"1";
+    System.out.println(str);
+  }
+
+}
